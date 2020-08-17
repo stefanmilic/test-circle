a73638f5372c7c645172268593d4d8e111723fd6
Stefan Milic <milicstefan6@gmail.com>
2020-08-17 12:01:20 +0200

    add coverage

--- services/bugCrowdService.js
+++ services/bugCrowdService.js
@@ -4,10 +4,10 @@ const BugCrowdEndpoint = 'https://api.bugcrowd.com';
 const headers = {
   Accept: 'application/vnd.bugcrowd+json',
   'Content-Type': 'application/json',
-  Authorization: `Token ${process.env.BUG_CROWD_TOKEN}`,
+  Authorization: `Token ${process.env.BUG_CROWD_TOKEN}`
 };
 
-module.exports.createSubmission = async (findings) => {
+module.exports.createSubmission = async findings => {
   // bounty_uuid 357a81ca-92b7-4b60-9548-5a64533c5cca
   // to get this uuid you should run first (GET) https://api.bugcrowd.com/bounties route
   const path = `/bounties/${process.env.BOUNTY_UUID}/submissions`;
@@ -20,14 +20,14 @@ module.exports.createSubmission = async (findings) => {
       priority: scoreCalculation(findings.score[0].score),
       substate: 'unresolved',
       extra_info_markdown: findings.definition.risk,
-      researcher_email: 'security@tx.group',
-    },
+      researcher_email: 'security@tx.group'
+    }
   };
 
   const res = await fetch(url, {
     method: 'POST',
     headers,
-    body: JSON.stringify(body),
+    body: JSON.stringify(body)
   });
 
   return res.json();
@@ -39,7 +39,7 @@ module.exports.getBounties = async () => {
 
   const res = await fetch(url, {
     method: 'GET',
-    headers,
+    headers
   });
   return res.json();
 };
@@ -56,3 +56,5 @@ function scoreCalculation(score) {
       return Math.floor(score);
   }
 }
+
+module.exports.scoreCalculation = scoreCalculation;
